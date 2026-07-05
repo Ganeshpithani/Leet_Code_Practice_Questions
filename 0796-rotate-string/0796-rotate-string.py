2f8278e287ee168f14bbf546695e06dd0c58079f
Pithani Ganesh <ganeshpithani001@gmail.com>
2026-07-05 11:22:19 +0530

    Time: 0 ms (100%), Space: 19.2 MB (51.06%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -3,8 +3,11 @@ class Solution:
         if len(s)!=len(goal):
             return False
         n=len(s)
+        r=[]
         for i in range(n):
-            if s==goal:
-                return True
+            r.append(s)
             s=s[1:]+s[0]
+        for j in r:
+            if j==goal:
+                return True
         return False
