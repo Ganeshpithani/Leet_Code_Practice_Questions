c21e29a19be4af5d4f3b58532bfca0733ecd27ec
Pithani Ganesh <ganeshpithani001@gmail.com>
2026-09-01 08:48:33 +0530

    Time: 40 ms (97.49%), Space: 29.3 MB (17.24%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -3,9 +3,10 @@ class Solution:
         m_x=sum(nums[:k])
         w_x=m_x
         for i in range(k,len(nums)):
-            w_x+=nums[i]
-            w_x-=nums[i-k]
-            m_x=max(m_x,w_x)
+            w_x=w_x-nums[i-k]+nums[i]
+            if w_x>m_x:
+                m_x=w_x
+            
         return m_x/k
 
         
